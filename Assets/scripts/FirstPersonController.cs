5892d26a1c99cd44aca83b4b70a139df9e6bef8e
Nigolukas <conico177@hotmail.com>
2025-09-23 14:56:45 -0500

    escenario dinos y movimiento personaje

---
+++ Assets/scripts/FirstPersonController.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.InputSystem; // Necesario para el nuevo Input System
+
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonControllerNew : MonoBehaviour
+{
+    [Header("Movimiento")]
+    public float speed = 5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1.5f;
+
+    [Header("Cámara")]
+    public Transform playerCamera;
+    public float mouseSensitivity = 100f;
+    public float verticalLookLimit = 80f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private float xRotation = 0f;
+
+    private PlayerInputActions inputActions;
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private bool jumpPressed;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        inputActions = new PlayerInputActions();
+
+        inputActions.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        inputActions.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        inputActions.Player.Look.performed += ctx => lookInput = ctx.ReadValue<Vector2>();
+        inputActions.Player.Look.canceled += ctx => lookInput = Vector2.zero;
+
+        inputActions.Player.Jump.performed += ctx => jumpPressed = true;
+    }
+
+    void OnEnable() => inputActions.Enable();
+    void OnDisable() => inputActions.Disable();
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Movimiento();
+        Camara();
+    }
+
+    void Movimiento()
+    {
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (controller.isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        if (jumpPressed /*&& controller.isGrounded*/)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            jumpPressed = false;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Camara()
+    {
+        float mouseX = lookInput.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = lookInput.y * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -verticalLookLimit, verticalLookLimit);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
